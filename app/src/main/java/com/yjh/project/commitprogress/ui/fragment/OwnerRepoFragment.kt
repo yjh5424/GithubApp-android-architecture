7d9647846b8f4221c38eaf50f63a931e326056c7
Yun Jung Hyeon <yjh54240@naver.com>
2018-07-08 23:36:20 +0900

    Modify layout_constraintVertical_bias

--- app/src/main/java/com/yjh/project/commitprogress/ui/fragment/OwnerRepoFragment.kt
+++ app/src/main/java/com/yjh/project/commitprogress/ui/fragment/OwnerRepoFragment.kt
@@ -3,7 +3,6 @@ package com.yjh.project.commitprogress.ui.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
